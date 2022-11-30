56a9407663531cfb88aee2bae5e412bd1ac6d194
Dexter Yanzheng Wu <sw1999@bu.edu>
2022-11-30 13:47:37 -0500

    notebook version update

--- env.py
+++ env.py
@@ -18,12 +18,29 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
+if gym.__version__ < '0.26':
+    env = gym.make('CartPole-v0', new_step_api=True,
+                   render_mode='single_rgb_array').unwrapped
+else:
+    env = gym.make('CartPole-v0', render_mode='rgb_array').unwrapped
 
-###replay memory class###
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
-#a named tuple representing a single transition in our environment. 
-#It essentially maps (state, action) pairs to their (next_state, reward) result, 
-#with the state being the screen difference image as described later on.
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+plt.ion()
+
+# if gpu is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+### replay memory class###
+Transition = namedtuple(
+    'Transition', ('state', 'action', 'next_state', 'reward'))
+# a named tuple representing a single transition in our environment.
+# It essentially maps (state, action) pairs to their (next_state, reward) result,
+# with the state being the screen difference image as described later on.
 
 
 class ReplayMemory(object):
@@ -31,8 +48,9 @@ class ReplayMemory(object):
     a cyclic buffer of bounded size that holds the transitions observed recently. 
     It also implements a .sample() method for selecting a random batch of transitions for training.
     """
+
     def __init__(self, capacity):
-        self.memory = deque([],maxlen=capacity)
+        self.memory = deque([], maxlen=capacity)
 
     def push(self, *args):
         """Save a transition"""
@@ -44,9 +62,11 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
-###end of replay memory class###
+### End of replay memory class###
 
-###DQN class###
+### DQN class###
+
+
 class DQN(nn.Module):
 
     def __init__(self, h, w, outputs):
@@ -60,8 +80,8 @@ class DQN(nn.Module):
 
         # Number of Linear input connections depends on output of conv2d layers
         # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
+        def conv2d_size_out(size, kernel_size=5, stride=2):
+            return (size - (kernel_size - 1) - 1) // stride + 1
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
         linear_input_size = convw * convh * 32
@@ -70,9 +90,59 @@ class DQN(nn.Module):
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, x):
-        x = x.to(torch.device)
+        x = x.to(device)
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
-###end of DQN class###
+### End of DQN class###
+
+
+### Start Input Extraction###
+"""
+The code below are utilities for extracting and processing rendered images from the environment. 
+It uses the torchvision package, which makes it easy to compose image transforms.
+"""
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+
+def get_cart_location(screen_width):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+
+
+def get_screen():
+    # Returned screen requested by gym is 400x600x3, but is sometimes larger
+    # such as 800x1200x3. Transpose it into torch order (CHW).
+    screen = env.render().transpose((2, 0, 1))
+    # Cart is in the lower half, so strip off the top and bottom of the screen
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(screen_width)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0)
+
+
+env.reset()
+plt.figure()
+plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),interpolation='none')
+plt.title('Example extracted screen')
+plt.show()
+### End of Start Input Extraction###
